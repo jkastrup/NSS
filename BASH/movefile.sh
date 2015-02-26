ebedb0933d2224068ae1ee39df1f4df4706007ae
Jason <jason.kastrup@gmail.com>
2015-02-26 16:20:32 -0500

    Week 3 Bash & Cron
    Added ‘movefile.sh’ (moves example picture ‘move_me.jpg’ from
    /startfolder to /endfolder)
    
    Copy of edited crontab in ‘crontab_copy.txt’.  Executes ‘move file.sh’
    every day at 4:00 am

---
+++ BASH/movefile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Basic bash script moving a file from dir startfolder to dir endfolder
+# based in /media/sf_/home/sharedfolder
+
+mv /media/sf_/home/sharedfolder/startfolder/move_me.jpg /media/sf_/home/sharedfolder/endfolder
+echo
+echo "Succesfully moved 'move_me.jpg' to /media/sf_/home/sharedfolder/endfolder"
+echo
